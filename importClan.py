63ee9f94d1ac919d196f2501e3fdd695e3bc18f2
EC2 Default User <ec2-user@ip-172-31-29-204.us-west-2.compute.internal>
2018-12-04 18:01:03 +0000

    added importClan.py

---
+++ importClan.py
@@ -0,0 +1,68 @@
+import requests
+import json
+import csv
+import sys
+import urllib
+import gspread
+import pymongo
+from oauth2client.service_account import ServiceAccountCredentials
+
+#This script will get current clan members from API, then insert them into our database
+
+#init mongodb
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["frost"]
+
+#Gdrive API
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive'
+]
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+#COC API
+
+filename = 'apikey'
+f = open(filename, 'r')
+apikey = f.read().strip()
+
+url = 'https://api.clashofclans.com/v1/clans/%239RPU22RU/members'
+url2 = 'https://api.clashofclans.com/v1/players/'
+headers = {
+    'Accept': 'application/json',
+    'Authorization': 'Bearer '+apikey
+    }
+r = requests.get(url, headers=headers)
+data = json.loads(r.text.encode('utf-8'))
+#outputFile = open("clan.csv", 'wb') #load csv file
+#output = csv.writer(outputFile) #create a csv.write
+
+sheet = client.open("Frost Wars")
+clanSheet = sheet.worksheet("Clan Roster")
+
+#Clear names
+cell_list = clanSheet.range('A2:D51')
+
+for cell in cell_list:
+    cell.value = ''
+#clanSheet.update_cells(cell_list)
+
+#outputFile.write(r.text.encode('utf-8'))
+#output.writerow(data[0].keys())  # header row
+#output.writerow(["Tag","Name","Town Hall Level","Donations"])
+index=0
+for row in data["items"]:
+    r2 = requests.get(url2 + urllib.quote(row["tag"]), headers=headers)
+    data2 = json.loads(r2.text.encode('utf-8'))
+    #output.writerow([row["tag"],row["name"].encode('utf-8'),data2["townHallLevel"],row["donations"]])
+
+    
+    cell_list[index].value=row["tag"]
+    cell_list[index+1].value=row["name"]
+    cell_list[index+2].value=data2["townHallLevel"]
+    cell_list[index+3].value=row["donations"]
+
+    index=index+4
+
+clanSheet.update_cells(cell_list)
